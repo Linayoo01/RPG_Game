5ae47dea525a8287a868adeda53359a0d6cdf8db
linayoo <ryussistudio@gmail.com>
2025-03-18 17:10:32 +0900

    새로운 데이터 추가

--- lib/file_handler.dart
+++ lib/file_handler.dart
@@ -11,7 +11,12 @@ class FileHandler {
       exit(1);
     }
     final data = file.readAsStringSync().split(',');
-    return Character("플레이어", int.parse(data[0]), int.parse(data[1]), int.parse(data[2]));
+    return Character(
+      "플레이어",
+      int.parse(data[0]),
+      int.parse(data[1]),
+      int.parse(data[2]),
+    );
   }
 
   static List<Monster> loadMonsters() {
@@ -23,14 +28,23 @@ class FileHandler {
     final lines = file.readAsLinesSync();
     return lines.map((line) {
       var data = line.split(',');
-      return Monster(data[0], int.parse(data[1]), Random().nextInt(int.parse(data[2])) + 1);
+      return Monster(
+        data[0],
+        int.parse(data[1]),
+        Random().nextInt(int.parse(data[2])) + 1,
+      );
     }).toList();
   }
 
   static void saveResult(Character character, bool isWin) {
-    final file = File('data/result.txt');
-    String result = "캐릭터: ${character.name}, 남은 체력: ${character.health}, 결과: ${isWin ? '승리' : '패배'}\n";
-    file.writeAsStringSync(result, mode: FileMode.append);
-    print("게임 결과가 저장되었습니다.");
-  }
+    try {
+      final file = File("data/result.txt");
+      String result =
+          "캐릭터: ${character.name}, 남은 체력: ${character.health}, 결과: ${isWin ? '승리' : '패배'}\n";
+      file.writeAsStringSync(result, mode: FileMode.append);
+      print("💾 게임 결과가 저장되었습니다.");
+    } catch (e) {
+      print("⚠️ 게임 결과 저장 중 오류 발생: $e");
+    } 
+  } 
 }
